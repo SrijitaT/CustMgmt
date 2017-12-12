c8cb5de54fd003204dd445aaa71e2aa3ccc8ed50
Srijita Thakur <34444499+SrijitaT@users.noreply.github.com>
2017-12-12 12:10:50 +0530

    Add files via upload

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
 
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Customer and Goal Mangement Project</h1>
+        <p>Project Objective: Setting Customer and Goal Listing</p>
+        <Link to="about" className="btn btn-primary btn-lg">About Us</Link>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
